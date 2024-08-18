18e8e8c2675d47bae9bcc95478019176dcb59730
matheusldev <mgdesenvolvedor@gmail.com>
2024-08-17 23:25:36 -0300

    feat: adicionando informações acadêmicas e profissionais ao portfólio

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,7 @@
 import ContainerSection from '../ContainerSection';
 import Matheus from '../../assets/images/matheus.png'
-import { ContainerBio, ContainerText, ImagePerson, LinkTalkMe } from './style';
+import { ContainerBio, ContainerContent, ContainerHistory, ContainerHistoryEducation, ContainerText, ImagePerson, LinkTalkMe, MenuAside } from './style';
+import Container from '../Container';
 
 const About = () => {
   return (
@@ -15,6 +16,77 @@ const About = () => {
           <LinkTalkMe href='https://wa.me/+5521980729950' target='_blank'>Let's Go!</LinkTalkMe>
         </ContainerText>
       </ContainerBio>
+      <ContainerHistory>
+        <MenuAside>
+          <ul>
+            <li>Formações</li>
+            <li>Experiência Profissional</li>
+          </ul>
+        </MenuAside>
+        <ContainerHistoryEducation>
+          <Container height='fit-content' borderRadius='.3rem'>
+            <ContainerContent>
+              <h2>Ensino Superior</h2>
+              <br/>
+              <h3>Instituição:</h3>
+              <p>Universidade Estácio de Sá - UNESA</p>
+              <br/>
+              <h3>Curso:</h3>
+              <p>Análise e Desenvolvimentos de Sistemas, cursando 4° período.</p>
+            </ContainerContent>
+          </Container>
+          <Container height='fit-content' borderRadius='.3rem'>
+            <ContainerContent>
+              <h2>Cursos</h2>
+              <br/>
+              <h3>Instituição:</h3>
+              <p>Origamid</p>
+              <br/>
+              <h3>Cursos Realizados:</h3>
+              <p>
+                <a href=''>JavaScript Completo</a>, <a href=''>TypeScript Completo</a>, <a href=''>React Completo</a>, <a href=''>React com TypeScript</a>, <a href=''>Redux com React</a> e <a href=''>UI Design</a>.
+              </p>
+              <br/><br/>
+              <h3>Instituição:</h3>
+              <p>Udemy</p>
+              <br/>
+              <h3>Cursos Realizados:</h3>
+              <p>
+                <a href=''>Desenvolvimento Web Completo</a> e <a href=''>NestJS</a>.
+              </p>
+              <br/><br/>
+              <h3>Instituição:</h3>
+              <p>Digital Innovation One - DIO</p>
+              <br/>
+              <h3>Cursos Realizados:</h3>
+              <p>
+                <a href=''>Git e GitHub</a>, <a href=''>Metodologia Ágil SCRUM</a>, e <a href=''>Bootcamp Cloud AWS</a>.
+              </p>
+            </ContainerContent>
+          </Container>
+          <Container height='fit-content' borderRadius='.3rem'>
+            <ContainerContent>
+              <h2>Experiência Profissional</h2>
+              <br/>
+              <h3>Empresa:</h3><h4>Markts.</h4>
+              <p> - <strong>Período:</strong> Fevereiro de 2024, até o presente momento.</p>
+              <br/>
+              <h3>Cargo:</h3>
+              <p>Desenvolvedor Full-Stack e Mobile.</p>
+              <br/>
+              <h3>Descrição:</h3>
+              <ul>
+                <li>Desenvolver aplicações web e móveis intuitivas e responsivas usando React, React Native e TypeScript;</li>
+                <li>Utilizar o NestJS para desenvolver a lógica do servidor;</li>
+                <li>Consumir WebServices em RestFull;</li>
+                <li>Usar o Git para controlar as versões do código-fonte;</li>
+                <li>Identificar e corrigir bugs em aplicações existentes, aplicar testes unitários e adotar Clean Code;</li>
+                <li>Usar a metodologia ágil Scrum.</li>
+              </ul>
+            </ContainerContent>
+          </Container>
+        </ContainerHistoryEducation>
+      </ContainerHistory>
     </ContainerSection>
   )
 }
--- src/components/About/style.ts
+++ src/components/About/style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Colors from '../../styles/Colors';
+import Fonts from '../../styles/Fonts';
 
 export const ContainerBio = styled.article`
   width: 100%;
@@ -7,6 +8,7 @@ export const ContainerBio = styled.article`
   border-radius: .3rem;
   border-top-left-radius: 7.5rem;
   box-shadow: 0 0 .3rem 0 rgba(0,0,0,.1);
+  margin-bottom: 2rem;
 `
 
 export const ImagePerson = styled.img`
@@ -29,7 +31,65 @@ export const LinkTalkMe = styled.a`
   width: fit-content;
   background: ${Colors.gradient};
   color: ${Colors.white};
+  font-family: ${Fonts.Poppins_bold};
+  font-weight: 700;
   font-size: 1.2rem;
   padding: 1rem 1.5rem;
   border-radius: .3rem;
 `
+
+export const ContainerHistory = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+`
+
+export const MenuAside = styled.aside`
+  ul li {
+    font-family: ${Fonts.Poppins_bold};
+    font-weight: 700;
+    font-size: 1.5rem;
+    padding: 1rem 1.5rem;
+    margin-bottom: 2rem;
+    border-radius: .3rem;
+    box-shadow: 0 0 .3rem 0 rgba(0,0,0,.1);
+  }
+`
+
+export const ContainerContent = styled.div`
+  width: 100%;
+  padding: 1rem;
+  color: ${Colors.white};
+
+  h3, h4, p {
+    display: inline;
+  }
+
+  h3 {
+    margin-right: .5rem;
+  }
+
+  a {
+    color: ${Colors.white};
+    font-size: 1rem;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  ul {
+    padding-left: 1rem;
+    li {
+      list-style: disc;
+    }
+  }
+`
+
+export const ContainerHistoryEducation = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem
+` 
