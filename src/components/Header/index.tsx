4374145978c41bf17cf16838ac1193f4ecf534a9
matheusldev <mgdesenvolvedor@gmail.com>
2024-08-24 20:19:38 -0300

    feat: adicionando scroll suave

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,39 @@
+import { useRef } from 'react';
 import { ContainerHeader, MenuNav } from './style';
 
 const Header = () => {
+  const headerRef = useRef<HTMLDivElement>(null);
+
+
+  const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, targetId: string) => {
+    event.preventDefault();
+    const element = document.getElementById(targetId);
+    
+    if (element && headerRef.current) {
+      const headerHeight = headerRef.current.offsetHeight;
+      const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = elementPosition - headerHeight;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  };
+
   return (
-    <ContainerHeader>
+    <ContainerHeader ref={headerRef}>
       <MenuNav>
         <ul>
-          <li><a href="#inicio" rel="noopener noreferrer">HOME</a></li>
-          <li><a href="#projeto" rel="noopener noreferrer">PROJETOS</a></li>
-          <li><a href="#sobre" rel="noopener noreferrer">SOBRE MIM</a></li>
+          <li>
+            <a href="#inicio" onClick={(e) => handleSmoothScroll(e, 'inicio')} rel="noopener noreferrer">HOME</a>
+          </li>
+          <li>
+            <a href="#projeto" onClick={(e) => handleSmoothScroll(e, 'projeto')} rel="noopener noreferrer">PROJETOS</a>
+          </li>
+          <li>
+            <a href="#sobre" onClick={(e) => handleSmoothScroll(e, 'sobre')} rel="noopener noreferrer">SOBRE MIM</a>
+          </li>
         </ul>
       </MenuNav>
     </ContainerHeader>
